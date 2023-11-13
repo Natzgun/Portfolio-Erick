00d4b66d929385d9838d4bffa26d358a46967c95
Natzgun <itzlilouxx@gmail.com>
2023-11-12 22:04:02 -0500

    Sections of skills finished

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,4 +1,5 @@
 import { BsLinkedin, BsGithub } from "react-icons/bs";
+import Skills from "./Skills";
 
 const About = () => {
   /*Aqui mi contenido centrado esta al minimo y puede reutilizarse*/
@@ -8,7 +9,7 @@ const About = () => {
       className="bg-gray-800 w-full h-full text-gray-300"
     >
       <div className="max-w-[1250px] mx-auto flex flex-col justify-center items-center">
-        <h2 className="text-3xl py-4 font-bold">About me</h2>
+        <h2 className="text-3xl py-2 my-4 font-bold border-t-4 border-pink-600">ABOUT ME</h2>
         <div className=" grid sm:grid-cols-2 grid-rows-2 w-9/12 gap-2">
           <div className="">
             <section className="flex flex-col justify-center h-full items-center">
@@ -37,42 +38,40 @@ const About = () => {
                 </ul>
               </div>
               <section className="w-full flex justify-center gap-4">
-                <div className="bg-slate-700 w-auto hover:bg-slate-800 cursor-pointer">
-                  <div className="p-4">
-                    <BsGithub size={20} className="text-green-500 mb-1" />
-                    <h4 className="text-lg">
-                      <strong>Github</strong>
-                    </h4>
-                    <p className="text-sm">
-                      View my all projects and activity here
-                    </p>
+                <a href="https://github.com/Natzgun" target="_blank">
+                  <div className="bg-slate-700 w-auto hover:bg-slate-800 cursor-pointer">
+                    <div className="p-4">
+                      <BsGithub size={20} className="text-green-500 mb-1" />
+                      <h4 className="text-lg">
+                        <strong>Github</strong>
+                      </h4>
+                      <p className="text-sm">
+                        View my all projects and activity here
+                      </p>
+                    </div>
                   </div>
-                </div>
-                <div className="bg-slate-700 w-auto hover:bg-slate-800 cursor-pointer">
-                  <div className="p-4">
-                    <BsLinkedin size={20} className="text-green-500 mb-1" />
-                    <h4 className="text-lg">
-                      <strong>LinkedIn</strong>
-                    </h4>
-                    <p className="text-sm">
-                      View my profile and contact with me here
-                    </p>
+                </a>
+                <a href="https://www.linkedin.com/in/erick-rmd" target="_blank">
+                  <div className="bg-slate-700 w-auto hover:bg-slate-800 cursor-pointer">
+                    <div className="p-4">
+                      <BsLinkedin size={20} className="text-green-500 mb-1" />
+                      <h4 className="text-lg">
+                        <strong>LinkedIn</strong>
+                      </h4>
+                      <p className="text-sm">
+                        View my profile and contact with me here
+                      </p>
+                    </div>
                   </div>
-                </div>
+                </a>
               </section>
             </div>
           </div>
           <div className="sm:col-span-2">
-            <h2 className="font-bold my-4 text-center"><strong className="border-t-4">SKILLS</strong></h2>
-            <div className="bg-pink-200">
-              Imagenes de los skills
-              <img
-                src="https://i.ytimg.com/vi/KEkrWRHCDQU/maxresdefault.jpg"
-                alt=""
-                className="p-4"
-              />
-            </div>
-            <div className="bg-pink-400">Mas skills</div>
+            <h2 className="font-bold my-4 text-center">
+              <strong className="border-t-4 py-2 border-pink-600 text-3xl font-bold">SKILLS</strong>
+            </h2>
+            <Skills/>
           </div>
         </div>
       </div>
---
+++ src/pages/About/Skills.jsx
@@ -0,0 +1,43 @@
+import HTML from "../../assets/html.png";
+import CSS from "../../assets/css.png";
+import JavaScript from "../../assets/javascript.png";
+import ReactImg from "../../assets/react.png";
+import Node from "../../assets/node.png";
+import FireBase from "../../assets/firebase.png";
+import GitHub from "../../assets/github.png";
+import Tailwind from "../../assets/tailwind.png";
+import Mongo from "../../assets/mongo.png";
+import CPP from  "../../assets/cpp.png";
+import Linux from "../../assets/tux.png";
+import Python from "../../assets/python.png";
+import Git from "../../assets/git.png";
+
+const Skills = () => {
+  const skillImages = [
+    { image: Linux, name: "Linux" },
+    { image: JavaScript, name: "JavaScript" },
+    { image: ReactImg, name: "React" },
+    { image: Node, name: "Node" },
+    // { image: FireBase, name: "FireBase" },
+    { image: Git, name: "Git" },
+    { image: GitHub, name: "GitHub" },
+    { image: CPP, name: "C++" },
+    { image: Python, name: "Python"},
+    { image: Tailwind, name: "Tailwind" },
+    { image: Mongo, name: "Mongo" },
+    { image: HTML, name: "HTML" },
+    { image: CSS, name: "CSS" },
+  ];
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+      {skillImages.map((skill) => (
+        <div className="shadow-md shadow-[#041c16] hover:scale-110 duration-500">
+          <img className="w-20 mx-auto" src={skill.image} alt="HTML icon" />
+          <p className="my-4">{skill.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Skills;
